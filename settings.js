59bab4aed43966b1bdb43c324d07d4f0c5062d9f
mads94i5 <mads94i5@stud.kea.dk>
2023-03-12 15:08:27 +0100

    sec

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://46.101.151.102:8080/api"
+export const API_URL = "http://bergandersen.com:8080/api/cars"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
