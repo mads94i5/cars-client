26cb8f8f238c871d9988b5421a671b09b1f44751
mads94i5 <mads94i5@stud.kea.dk>
2023-03-12 14:54:52 +0100

    sec

--- pages/members/members.js
+++ pages/members/members.js
@@ -1,6 +1,18 @@
 import { API_URL } from "../../settings.js"
+import { fetchGetJson } from "../../utils.js"
 const URL = API_URL + "/members"
 
-export function initMembers(){
+export async function initMembers(){
+    const tbody = document.getElementById("table-rows");
+    const members = await fetchGetJson(URL);
+    const rows = members.map(member => `
+      <tr>
+        <td>${member.username}</td>
+        <td>${member.email}</td>
+        <td>${member.firstName} + " " + ${member.lastName}</td>
+        <td>${member.ranking}</td>
+      </tr>
+    `);
+    tbody.innerHTML = rows.join("");
 
 }
