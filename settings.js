1021db6029eabe8a12e1d04f48520723afb45302
Nicolai Andersen <niiicolai@gmail.com>
2023-03-12 18:30:29 +0100

    use new endpoint

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://bergandersen.com:8080/api/cars"
+export const API_URL = "https://mads-api.bergandersen.com/api/cars"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
