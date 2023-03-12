bc189891b0201630797a68b8a51c608338fda76d
mads94i5 <mads94i5@stud.kea.dk>
2023-03-12 14:58:35 +0100

    sec

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080/api"
+export const API_URL = "http://46.101.151.102:8080/api"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
