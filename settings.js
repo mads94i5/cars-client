7245e13d09373abbc174e4b95968bc21c034ef86
mads94i5 <mads94i5@stud.kea.dk>
2023-04-09 17:45:39 +0200

    backend update

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://api.swalkstudios.com/api/"
+export const API_URL = "https://cars-r-us123.azurewebsites.net/api/"
 // export const API_URL = "https://localhost:8080/api/"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
